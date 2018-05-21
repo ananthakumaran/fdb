2100325afb34f241aa9dacc11faec79c5e0ec191
Anantha Kumaran <ananthakumaran@gmail.com>
2018-05-21 22:34:51 +0530

    fix version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule FDB.MixProject do
       app: :fdb,
       compilers: [:nif] ++ Mix.compilers(),
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
