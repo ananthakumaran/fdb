df0ea7be94acf94f143c62aec0a4295db41bd5bf
Anantha Kumaran <ananthakumaran@gmail.com>
2018-05-27 18:01:31 +0530

    add nullable

---
+++ lib/fdb/coder/nullable.ex
@@ -0,0 +1,17 @@
+defmodule FDB.Coder.Nullable do
+  @behaviour FDB.Coder.Behaviour
+
+  def new(coder) do
+    %FDB.Coder{module: __MODULE__, opts: coder}
+  end
+
+  @code <<0x00>>
+
+  @impl true
+  def encode(nil, coder), do: @code
+  def encode(value, coder), do: coder.module.encode(value, coder.opts)
+
+  @impl true
+  def decode(@code <> rest, _), do: {nil, rest}
+  def decode(value, coder), do: coder.module.decode(value, coder.opts)
+end
---
+++ test/fdb/coder/nullable_test.exs
@@ -0,0 +1,16 @@
+defmodule FDB.Coder.NullableTest do
+  alias FDB.Coder.Nullable
+  alias FDB.Coder.ByteString
+  import TestUtils
+
+  use ExUnit.Case
+  use ExUnitProperties
+
+  property "encode / decode" do
+    check all binaries <- list_of(one_of([constant(nil), binary()])),
+              prefix <- binary() do
+      coder = Nullable.new(ByteString.new())
+      assert_coder_order_symmetry(coder, binaries)
+    end
+  end
+end
