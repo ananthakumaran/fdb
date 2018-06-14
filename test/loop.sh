7287d4f11a68614b0c4e848cc09443a0a1e02122
Anantha Kumaran <ananthakumaran@gmail.com>
2018-06-14 19:05:15 +0530

    update build scripts

--- test/loop.sh
+++ test/loop.sh
@@ -8,8 +8,8 @@ do
     echo ""
 
     echo "# api"
-    ../foundationdb/bindings/bindingtester/bindingtester.py elixir --test-name api --num-ops 1000 --compare python || exit 1
+    foundationdb/bindings/bindingtester/bindingtester.py elixir --test-name api --num-ops 1000 --compare python || exit 1
 
     echo "# api with concurrency 5"
-    ../foundationdb/bindings/bindingtester/bindingtester.py elixir --test-name api --num-ops 1000 --concurrency 5 || exit 1
+    foundationdb/bindings/bindingtester/bindingtester.py elixir --test-name api --num-ops 1000 --concurrency 5 || exit 1
 done
