2d42de72ebcbc262854113e33ea0a61e4cccf89c
Anantha Kumaran <ananthakumaran@gmail.com>
2018-06-14 16:34:15 +0530

    run in travis

---
+++ test/loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set +e
+for round in {1..10}
+do
+    echo "Round $round"
+    echo "========"
+    echo ""
+
+    echo "# api"
+    ../foundationdb/bindings/bindingtester/bindingtester.py elixir --test-name api --num-ops 1000 --compare python || exit 1
+
+    echo "# api with concurrency 5"
+    ../foundationdb/bindings/bindingtester/bindingtester.py elixir --test-name api --num-ops 1000 --concurrency 5 || exit 1
+done
