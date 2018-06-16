607c0f8d10b3d2d8f27aac22debc299e2f534cb5
Anantha Kumaran <ananthakumaran@gmail.com>
2018-06-16 14:13:06 +0530

    setup dialyzer

--- mix.exs
+++ mix.exs
@@ -8,7 +8,11 @@ defmodule FDB.MixProject do
       version: "0.1.0",
       elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_deps: :transitive,
+        flags: [:unmatched_returns, :race_conditions, :error_handling, :underspecs]
+      ]
     ]
   end
 
@@ -22,7 +26,8 @@ defmodule FDB.MixProject do
     [
       {:sweet_xml, "~> 0.6"},
       {:stream_data, "~> 0.4", only: :test},
-      {:ex_doc, "~> 0.18", only: :dev}
+      {:ex_doc, "~> 0.18", only: :dev},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end
