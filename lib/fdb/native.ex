c0aa2ec1a04274822a565dc9d8fdc34c1cb27f2e
Anantha Kumaran <ananthakumaran@gmail.com>
2018-06-16 13:15:04 +0530

    fix dialyzer warnings

--- lib/fdb/native.ex
+++ lib/fdb/native.ex
@@ -10,35 +10,52 @@ defmodule FDB.Native do
     :ok = :erlang.load_nif(path, 0)
   end
 
-  def get_max_api_version, do: exit(:nif_library_not_loaded)
+  def get_max_api_version, do: :erlang.nif_error(:nif_library_not_loaded)
 
   def select_api_version_impl(_runtime_version, _header_version),
-    do: exit(:nif_library_not_loaded)
+    do: :erlang.nif_error(:nif_library_not_loaded)
 
-  def network_set_option(_option), do: exit(:nif_library_not_loaded)
-  def network_set_option(_option, _value), do: exit(:nif_library_not_loaded)
-  def setup_network, do: exit(:nif_library_not_loaded)
-  def run_network, do: exit(:nif_library_not_loaded)
-  def stop_network, do: exit(:nif_library_not_loaded)
-  def create_cluster(_file_path), do: exit(:nif_library_not_loaded)
-  def cluster_set_option(_cluster, _option), do: exit(:nif_library_not_loaded)
-  def cluster_set_option(_cluster, _option, _value), do: exit(:nif_library_not_loaded)
-  def cluster_create_database(_cluster), do: exit(:nif_library_not_loaded)
-  def database_set_option(_database, _option), do: exit(:nif_library_not_loaded)
-  def database_set_option(_database, _option, _value), do: exit(:nif_library_not_loaded)
-  def database_create_transaction(_database), do: exit(:nif_library_not_loaded)
-  def transaction_set_option(_transaction, _option), do: exit(:nif_library_not_loaded)
-  def transaction_set_option(_transaction, _option, _value), do: exit(:nif_library_not_loaded)
-  def transaction_get(_transaction, _key, _snapshot), do: exit(:nif_library_not_loaded)
-  def transaction_get_read_version(_transaction), do: exit(:nif_library_not_loaded)
-  def transaction_get_committed_version(_transaction), do: exit(:nif_library_not_loaded)
-  def transaction_get_versionstamp(_transaction), do: exit(:nif_library_not_loaded)
-  def transaction_watch(_transaction, _key), do: exit(:nif_library_not_loaded)
+  def network_set_option(_option), do: :erlang.nif_error(:nif_library_not_loaded)
+  def network_set_option(_option, _value), do: :erlang.nif_error(:nif_library_not_loaded)
+  def setup_network, do: :erlang.nif_error(:nif_library_not_loaded)
+  def run_network, do: :erlang.nif_error(:nif_library_not_loaded)
+  def stop_network, do: :erlang.nif_error(:nif_library_not_loaded)
+  def create_cluster(_file_path), do: :erlang.nif_error(:nif_library_not_loaded)
+  def cluster_set_option(_cluster, _option), do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def cluster_set_option(_cluster, _option, _value),
+    do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def cluster_create_database(_cluster), do: :erlang.nif_error(:nif_library_not_loaded)
+  def database_set_option(_database, _option), do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def database_set_option(_database, _option, _value),
+    do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def database_create_transaction(_database), do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def transaction_set_option(_transaction, _option),
+    do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def transaction_set_option(_transaction, _option, _value),
+    do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def transaction_get(_transaction, _key, _snapshot),
+    do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def transaction_get_read_version(_transaction), do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def transaction_get_committed_version(_transaction),
+    do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def transaction_get_versionstamp(_transaction), do: :erlang.nif_error(:nif_library_not_loaded)
+  def transaction_watch(_transaction, _key), do: :erlang.nif_error(:nif_library_not_loaded)
 
   def transaction_get_key(_transaction, _key, _or_equal, _offset, _snapshot),
-    do: exit(:nif_library_not_loaded)
+    do: :erlang.nif_error(:nif_library_not_loaded)
 
-  def transaction_get_addresses_for_key(_transaction, _key), do: exit(:nif_library_not_loaded)
+  def transaction_get_addresses_for_key(_transaction, _key),
+    do: :erlang.nif_error(:nif_library_not_loaded)
 
   def transaction_get_range(
         _transaction,
@@ -55,26 +72,31 @@ defmodule FDB.Native do
         _snapshot,
         _reverse
       ),
-      do: exit(:nif_library_not_loaded)
+      do: :erlang.nif_error(:nif_library_not_loaded)
 
-  def transaction_set(_transaction, _key, _value), do: exit(:nif_library_not_loaded)
-  def transaction_set_read_version(_transaction, _version), do: exit(:nif_library_not_loaded)
+  def transaction_set(_transaction, _key, _value), do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def transaction_set_read_version(_transaction, _version),
+    do: :erlang.nif_error(:nif_library_not_loaded)
 
   def transaction_add_conflict_range(_transaction, _begin_key, _end_key, _conflict_range_type),
-    do: exit(:nif_library_not_loaded)
+    do: :erlang.nif_error(:nif_library_not_loaded)
 
   def transaction_atomic_op(_transaction, _key, _param, _operation_type),
-    do: exit(:nif_library_not_loaded)
+    do: :erlang.nif_error(:nif_library_not_loaded)
 
-  def transaction_clear(_transaction, _key), do: exit(:nif_library_not_loaded)
+  def transaction_clear(_transaction, _key), do: :erlang.nif_error(:nif_library_not_loaded)
 
   def transaction_clear_range(_transaction, _begin_key, _end_key),
-    do: exit(:nif_library_not_loaded)
+    do: :erlang.nif_error(:nif_library_not_loaded)
 
-  def transaction_commit(_transaction), do: exit(:nif_library_not_loaded)
-  def transaction_cancel(_transaction), do: exit(:nif_library_not_loaded)
-  def transaction_on_error(_transaction, _error_code), do: exit(:nif_library_not_loaded)
-  def get_error(_code), do: exit(:nif_library_not_loaded)
-  def get_error_predicate(_predicate_test, _code), do: exit(:nif_library_not_loaded)
-  def future_resolve(_future, _reference), do: exit(:nif_library_not_loaded)
+  def transaction_commit(_transaction), do: :erlang.nif_error(:nif_library_not_loaded)
+  def transaction_cancel(_transaction), do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def transaction_on_error(_transaction, _error_code),
+    do: :erlang.nif_error(:nif_library_not_loaded)
+
+  def get_error(_code), do: :erlang.nif_error(:nif_library_not_loaded)
+  def get_error_predicate(_predicate_test, _code), do: :erlang.nif_error(:nif_library_not_loaded)
+  def future_resolve(_future, _reference), do: :erlang.nif_error(:nif_library_not_loaded)
 end
