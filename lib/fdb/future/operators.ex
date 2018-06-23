2ac49f30e60c305ef8618f99139e078563ee2a3e
Anantha Kumaran <ananthakumaran@gmail.com>
2018-06-23 10:35:12 +0530

    fix warning

--- lib/fdb/future/operators.ex
+++ lib/fdb/future/operators.ex
@@ -1,7 +1,7 @@
 defmodule FDB.Future.Operators do
   def @future, do: FDB.Future.resolve(future)
 
-  defmacro __using__(opts) do
+  defmacro __using__(_opts) do
     quote do
       import Kernel, except: [@: 1]
       import unquote(__MODULE__)
