92c7a2581aa9b69268aea5a64f658dbb3ca7f42e
Anantha Kumaran <ananthakumaran@gmail.com>
2018-07-01 11:56:12 +0530

    use pattern match

--- lib/fdb/transaction/coder.ex
+++ lib/fdb/transaction/coder.ex
@@ -1,31 +1,33 @@
 defmodule FDB.Transaction.Coder do
+  alias FDB.Coder
+
   defstruct key: FDB.Coder.Identity.new(), value: FDB.Coder.Identity.new()
 
   @type t :: %__MODULE__{key: FDB.Coder.t(), value: FDB.Coder.t()}
 
   @doc false
   @spec encode_key(t, any) :: any
-  def encode_key(coder, key) do
-    coder.key.module.encode(key, coder.key.opts)
+  def encode_key(%__MODULE__{key: %Coder{module: module, opts: opts}}, key) do
+    module.encode(key, opts)
   end
 
   @doc false
   @spec decode_key(t, any) :: any
-  def decode_key(coder, key) do
-    {value, <<>>} = coder.key.module.decode(key, coder.key.opts)
+  def decode_key(%__MODULE__{key: %Coder{module: module, opts: opts}}, key) do
+    {value, <<>>} = module.decode(key, opts)
     value
   end
 
   @doc false
   @spec encode_value(t, any) :: any
-  def encode_value(coder, key) do
-    coder.value.module.encode(key, coder.value.opts)
+  def encode_value(%__MODULE__{value: %Coder{module: module, opts: opts}}, key) do
+    module.encode(key, opts)
   end
 
   @doc false
   @spec decode_value(t, any) :: any
-  def decode_value(coder, key) do
-    {value, <<>>} = coder.value.module.decode(key, coder.value.opts)
+  def decode_value(%__MODULE__{value: %Coder{module: module, opts: opts}}, key) do
+    {value, <<>>} = module.decode(key, opts)
     value
   end
 
@@ -35,13 +37,13 @@ defmodule FDB.Transaction.Coder do
     encode_key(coder, key)
   end
 
-  def encode_range(coder, key, :first) do
-    {value, _} = coder.key.module.range(key, coder.key.opts)
+  def encode_range(%__MODULE__{key: %Coder{module: module, opts: opts}}, key, :first) do
+    {value, _} = module.range(key, opts)
     value <> <<0x00>>
   end
 
-  def encode_range(coder, key, :last) do
-    {value, _} = coder.key.module.range(key, coder.key.opts)
+  def encode_range(%__MODULE__{key: %Coder{module: module, opts: opts}}, key, :last) do
+    {value, _} = module.range(key, opts)
     value <> <<0xFF>>
   end
 end
