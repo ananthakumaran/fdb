d7300da67512eb0541a34b6403d5fca47499c0ed
Anantha Kumaran <ananthakumaran@gmail.com>
2018-11-20 11:55:41 +0530

    change version number

--- test/fdb/native_test.exs
+++ test/fdb/native_test.exs
@@ -2,15 +2,23 @@ defmodule FDB.NativeTest do
   use ExUnit.Case, async: false
   import FDB.Native
 
+  @current 600
+
   test "get_max_api_version" do
-    assert get_max_api_version() >= 510
+    assert get_max_api_version() >= @current
   end
 
   test "select_api_version_impl" do
-    assert_raise ErlangError, ~r/runtime_version/, fn -> select_api_version_impl("hello", 510) end
-    assert_raise ErlangError, ~r/header_version/, fn -> select_api_version_impl(510, "hello") end
-    assert select_api_version_impl(510, 510)
-    assert select_api_version_impl(600, 510) == 2201
+    assert_raise ErlangError, ~r/runtime_version/, fn ->
+      select_api_version_impl("hello", @current)
+    end
+
+    assert_raise ErlangError, ~r/header_version/, fn ->
+      select_api_version_impl(@current, "hello")
+    end
+
+    assert select_api_version_impl(@current, @current)
+    assert select_api_version_impl(@current + 100, @current) == 2201
   end
 
   test "get_error" do
